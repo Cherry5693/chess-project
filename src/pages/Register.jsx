9dce7c2d2c8440cf4853db41aa6288053e8b5e00
Cherry5693 <madhusudhan61007@gmail.com>
2025-03-20 21:50:44 +0530

    changed Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("http://localhost:5000/register", formData);
+            const res = await axios.post("https://chess-project-jvvt.onrender.com/register", formData);
             alert(res.data.message);
             navigate("/login");
         } catch (err) {
